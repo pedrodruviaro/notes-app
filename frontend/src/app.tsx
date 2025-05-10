e9658dfca4b6327724b2a37f097e11af378087db
pedrodruviaro <pedrodruviaro@gmail.com>
2025-05-10 08:21:22 -0300

    feat: init frontend widh shadcn

---
+++ frontend/src/app.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+export function App() {
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
